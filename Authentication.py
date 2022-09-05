0684fb7bb57c7971a27317a2063637f3e66473c7
Arafa <karimomar71@gmail.com>
2022-09-05 18:57:15 +0200

    push test

--- Authentication.py
+++ Authentication.py
@@ -1,40 +1,45 @@
+from ast import Return
 import time
 from MianPage import mainPage
 
 def entryInputValidation():
     x = input("Enter Your Choice: ")
-    if (x.isdigit() and int(x) in [1, 2]):
+    if ((x.isdigit() == True) and int(x) in [1, 2]):
         return int(x)
-    return entryInputValidation()
+    else :
+        print ("Invalid Input")
+        return entryInputValidation()
 
 
-# -------------------------- input functions ---------------------------------
-# -------------------------- check name validation ----------------------------
+#User Names#####################
 def enterFName():
     x = input("Enter your First Name: ")
-    if (x.isalpha() or not x):
+    if (x.isalpha() == True):
         return x
     else:
+        print("Invalid Input")
         return enterFName()
 
 
 def enterSName():
     x = input("Enter your second Name: ")
-    if (x.isalpha() or not x):
+    if (x.isalpha() == True):
         return x
     else:
+        print("Invalid Input")
         return enterSName()
 
 
-# -------------------------------- email input and validation -----------------------
+#Email############################
 import re
 
 
 def enteremail():
     x = input("Enter email: ")
-    if emailvalidator(x):
+    if (emailvalidator(x) == True):
         return x
     else:
+        print("Invalid Input")
         return enteremail()
 
 
@@ -46,39 +51,29 @@ def emailvalidator(email):
         return False
 
 
-# -------------------------------- Password  ---------------------------------------------
+#Password####################################33
 
 def enterPassword():
-    x = input("Enter Password: ")
-    if (len(x) < 8 or not x):
+    password1=input("enter your password")
+    password2=input("confirm your password")
+    if (re.fullmatch(password1,password2)):
+        return password1
+    else :
+        print("Password are not matched")
         return enterPassword()
-    else:
-        Confirmed_password = Confirm_password(x)
-        if Confirmed_password:
-            return x
-        else:
-            return enterPassword()
 
-
-def Confirm_password(Password):
-    x = input("Enter Password Again: ")
-    if (Password == x or not x):
-        return True
-    else:
-        return False
-
-
-# ------------------------------- phone --------------------------------------------------
+#Phone############################33
 
 def enterPhone():
     x = input("Enter Your Phone: ")
     if (re.match(r"^01[0-2,5]\d{1,8}$", x)):
         return x
     else:
+        print("Invalid Input")
         return enterPhone()
 
 
-# -------------------------- savedata ---------------------------
+#  Saved Data ###############################
 
 def saveData(data):
     file = open('usersdata.txt', 'a')
@@ -93,7 +88,7 @@ def Registration():
     Password = enterPassword()
     phone = enterPhone()
     id = round(time.time())
-    data = f"{id}:{email}:{Password}:{FirstName}:{secondName}:{phone}\n"
+    data = f"{id}:{email}:{Password}:{FirstName}:{secondName}:{phone}'\n'"
     saveData(data)
 
 def checkExistans(email,password):
